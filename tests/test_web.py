00956d6d41c121631be53cd1d60e287b6875f4dc
David Tang <dev@blueur.com>
2024-09-30 10:35:45 +0200

    feat: remove class

--- tests/test_web.py
+++ tests/test_web.py
@@ -4,65 +4,67 @@ from os import path
 from subprocess import check_output
 
 
-class TestWeb:
-    def test_index(self):
-        assert path.isfile("index.html"), "Il faut le fichier 'index.html'."
+def test_index(self):
+    assert path.isfile("index.html"), "Il faut le fichier 'index.html'."
 
-    def test_html(self):
-        for file in glob("**/*.html", recursive=True):
-            output = check_output(
-                [
-                    "curl",
-                    "-s",
-                    "-H",
-                    "Content-Type: text/html",
-                    "--data-binary",
-                    f"@{file}",
-                    "https://validator.w3.org/nu/?out=gnu&level=error",
-                ],
-            ).decode()
-            assert "" == output, f"{file} {output}"
 
-    def test_css(self):
-        count: int = 0
-        for file in glob("**/*.css", recursive=True):
-            count += 1
-            output = check_output(
-                [
-                    "curl",
-                    "-s",
-                    "-H",
-                    "Content-Type: text/css",
-                    "--data-binary",
-                    f"@{file}",
-                    "https://validator.w3.org/nu/?out=gnu",
-                ],
-            ).decode()
-            assert "" == output, f"{file} {output}"
-        assert count > 0, "Il faut au moins un fichier CSS."
+def test_html(self):
+    for file in glob("**/*.html", recursive=True):
+        output = check_output(
+            [
+                "curl",
+                "-s",
+                "-H",
+                "Content-Type: text/html",
+                "--data-binary",
+                f"@{file}",
+                "https://validator.w3.org/nu/?out=gnu&level=error",
+            ],
+        ).decode()
+        assert "" == output, f"{file} {output}"
 
-    # def test_tags(self):
-    #     any_tags: set[str] = set()
-    #     all_tags: set[str] = set()
 
-    #     class MyHTMLParser(HTMLParser):
-    #         inner_tags: set[str] = set()
+def test_css(self):
+    count: int = 0
+    for file in glob("**/*.css", recursive=True):
+        count += 1
+        output = check_output(
+            [
+                "curl",
+                "-s",
+                "-H",
+                "Content-Type: text/css",
+                "--data-binary",
+                f"@{file}",
+                "https://validator.w3.org/nu/?out=gnu",
+            ],
+        ).decode()
+        assert "" == output, f"{file} {output}"
+    assert count > 0, "Il faut au moins un fichier CSS."
 
-    #         def handle_starttag(self, tag, _):
-    #             any_tags.add(tag)
-    #             self.inner_tags.add(tag)
 
-    #     parser = MyHTMLParser()
-    #     for file in glob("**/*.html", recursive=True):
-    #         parser.inner_tags.clear()
-    #         parser.feed(open(file).read())
-    #         if all_tags:
-    #             all_tags &= parser.inner_tags
-    #         else:
-    #             all_tags = parser.inner_tags.copy()
+# def test_tags(self):
+#     any_tags: set[str] = set()
+#     all_tags: set[str] = set()
 
-    #     assert "title" in all_tags, "Il faut un titre (onglet) dans chaque fichier."
-    #     assert "h1" in all_tags, "Il faut un titre de niveau 1 dans chaque fichier."
-    #     assert "p" in all_tags, "Il faut au moins un paragraphe dans chaque fichier."
+#     class MyHTMLParser(HTMLParser):
+#         inner_tags: set[str] = set()
 
-    #     assert "img" in any_tags, "Il faut au moins une image dans le projet."
+#         def handle_starttag(self, tag, _):
+#             any_tags.add(tag)
+#             self.inner_tags.add(tag)
+
+#     parser = MyHTMLParser()
+#     for file in glob("**/*.html", recursive=True):
+#         parser.inner_tags.clear()
+#         parser.feed(open(file).read())
+#         if all_tags:
+#             all_tags &= parser.inner_tags
+#         else:
+#             all_tags = parser.inner_tags.copy()
+
+#     assert "title" in all_tags, "Il faut un titre (onglet) dans chaque fichier."
+#     assert "h1" in all_tags, "Il faut un titre de niveau 1 dans chaque fichier."
+#     assert "p" in all_tags, "Il faut au moins un paragraphe dans chaque fichier."
+
+#     assert "img" in any_tags, "Il faut au moins une image dans le projet."
